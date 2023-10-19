e4f4bde5e40134f712f4e7d5ec96761b4e10459e
jacobperry <perryjacob@outlook.com>
2023-10-18 18:02:56 -0700

    Updated Animation logic Switch to load all images from folder into a vector of handles.

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ fn main() {
             })
         )
         .add_plugins((
-            RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(1.0),
+            RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0),
             RapierDebugRenderPlugin::default()))
         .add_systems(Startup, (setup_camera, setup_physics, setup))
         .add_systems(Update, (animation, bevy::window::close_on_esc, player_input, apply_forces))
@@ -65,41 +65,59 @@ fn setup (
    // TODO: Move animation loading into its own function and create idle animation that can be triggered when the player is not moving the character
 
     let player_animations = AnimationNode { position: 0};
-    // player_animations.states.insert(String::from("running"), AnimationState { path: String::from("Test Running Animation.png"), dimensions: Vec2::new(35.0, 75.0), start_frame: 0, current_frame: 0, end_frame: 4 });
+    let swordfish_texture_handle = asset_server.load_folder("sword_fish");
+    let swordfish_texture_handle = swordfish_texture_handle.unwrap();
 
-    let texture_handle = asset_server.load("Test Running Animation.png");
-    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(35.0, 75.0), 4, 1, None, None);
-    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    let mut texture_atlas_handles: Vec<Handle<TextureAtlas>> = Vec::new();
 
+    for texture in swordfish_texture_handle
+    {
+        let typed_texture = texture.typed::<Image>();
+        let texture_atlas = TextureAtlas::from_grid(typed_texture, Vec2::new(48., 48.), 4, 1, None, None);
+        let atlas_handle = texture_atlases.add(texture_atlas);
+        texture_atlas_handles.push(atlas_handle);
+    }
 
+    let current_texture = texture_atlas_handles[SwordFishStates::Walk as usize].clone();
     let texture_atlas_sprite = TextureAtlasSprite::new(1);
 
     commands.spawn((
         SpriteSheetBundle {
             // transform: Transform::from_scale(Vec3::splat(1.0)),
             transform: Transform {
-                scale: Vec3::splat(1.0),
-                translation: Vec3::new(0.0, -290.0, 0.0),
+                scale: Vec3::splat(2.0),
+                translation: Vec3::new(0.0, 0.0, 0.0),
                 ..default()
             },
-            texture_atlas: texture_atlas_handle,
+            texture_atlas: current_texture,
             sprite: texture_atlas_sprite,
             ..default()
         },
-        Collider::cuboid(17.5, 37.5),
+        Collider::cuboid(24., 12.),
         RigidBody::Dynamic,
-        Restitution::coefficient(0.5),
-        ColliderMassProperties::Mass(150.0),
-        GravityScale(30.0),
+        ColliderMassProperties::Mass(1000.0),
+        GravityScale(5.0),
         Damping {
             linear_damping: 1.0,
             angular_damping: 1.0
+        },
+        Friction {
+            coefficient: 0.1,
+            ..default()
         },
         Velocity::default(),
         LockedAxes::ROTATION_LOCKED,
         AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
         player_animations
     ));
+}
+
+enum SwordFishStates {
+    Attack = 0,
+    Death = 1,
+    Hurt = 2,
+    Idle = 3,
+    Walk = 4
 }
 
 #[derive(Component, Deref, DerefMut)]
@@ -138,12 +156,12 @@ fn player_input (
 
     for mut vel in velocities.iter_mut() {
         if input.just_pressed(KeyCode::Space) {
-            vel.linvel.y = 800.0;
+            vel.linvel.y = 300.0;
         }
         if input.pressed(KeyCode::D) {
-            vel.linvel.x = 500.0;
+            vel.linvel.x = 300.0;
         } else if input.pressed(KeyCode::A) {
-            vel.linvel.x = -500.0;
+            vel.linvel.x = -300.0;
         }
     }
 
